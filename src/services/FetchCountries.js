a53fdf309c06eca61cfb68e8a17b6eb4f51eb042
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-17 17:31:10 -0800

    did my ReadMe to try and do some planning. Added supabase schema of what the table looks like as well as my miro board and planned out what I think my work flow will look like. I also pseudocoded my fetchCountries function. Going to implement that next and console.log to see if it's working!

---
+++ src/services/FetchCountries.js
@@ -0,0 +1,5 @@
+// I will have a fetch functions function here
+// export default function fetchCountries() {
+    // const response = await client.from('countries').select('*');
+    // return (response);
+// }
